2beb83b96c7e7a01713dcfa6b82648cabaf67067
KezaShania <kezashania5@gmail.com>
2025-12-15 17:16:49 +0100

    Updated the URL in predictions controller

--- backend/MediPredict/Controllers/Api/PredictionsController.cs
+++ backend/MediPredict/Controllers/Api/PredictionsController.cs
@@ -84,7 +84,7 @@ namespace MediPredict.Controllers.Api
                         var jsonContent = new StringContent(JsonSerializer.Serialize(aiPayload), Encoding.UTF8, "application/json");
 
                         // Call localhost:5001 (Since Python is running in the same Railway service)
-                        var response = await client.PostAsync("medipredict-ai-production-0b620.up.railway.app", jsonContent);
+                        var response = await client.PostAsync("https://medipredict-ai-production-0b620.up.railway.app/predict", jsonContent);
                         
                         if (response.IsSuccessStatusCode)
                         {
